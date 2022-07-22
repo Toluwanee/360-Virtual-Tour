2ce21681beaab5a507a9a6ffd300878bb119107b
Tolu <toluwalope9@gmail.com>
2022-07-22 11:27:08 +0100

    Initial commit

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject m_currentGate;
+    public GameObject m_nextGate;
+    void ToggleGate()
+    {
+        m_currentGate.SetActive(false);
+        m_nextGate.SetActive(true);
+    }
+}
